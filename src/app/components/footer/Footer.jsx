a8a5dbf7dce2aa890cafa458aaab42c84561fffe
Movindu Gunarathna <movindugunarathna@gmail.com>
2024-01-29 19:38:22 +0530

    navbar fixed and footer updated

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,14 +1,16 @@
+import Image from 'next/image';
+
 const Footer = () => {
   return (
     <footer className="text-black py-8 mt-10">
       <div className="md:w-1/4 text-center md:text-left">
-        <h2 className="text-xl font-bold mb-4">Connect with us</h2>
-        <p>Need more information? Write your concern to us and our specialist will get back to you.</p>
+        <h2 className="text-xl font-bold mb-4">Need more information?</h2>
+        <p>Write your concern to us and our specialist will get back to you.</p>
       </div>
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap">
           <div className="w-full md:w-1/4 text-center md:text-left">
-            <h2 className="text-xl font-bold mb-4">Land Pulse</h2>
+            <Image src="/logo.png" alt="logo" width={300} height={300} />
             <p className="mb-4">Where Visions Find Ground: Pulse Your Property Dreams with Us!</p>
           </div>
           <div className="w-full md:w-1/4 text-center md:text-left">
