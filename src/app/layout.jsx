5119e88870b245d2cd39e3d839fde7df55347861
Pasindu Geevinda <105873753+LuciferDIot@users.noreply.github.com>
2024-02-09 18:05:18 +0530

    signup added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,26 +1,26 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'LandPulse',
-  description:
-    "LandPulse helps users estimate current land values in the Colombo district's cities and predicts potential growth in the coming years. The app provides actionable insights for investors, professionals, and urban planners by integrating machine learning models and real estate data.",
+    title: "LandPulse",
+    description:
+        "LandPulse helps users estimate current land values in the Colombo district's cities and predicts potential growth in the coming years. The app provides actionable insights for investors, professionals, and urban planners by integrating machine learning models and real estate data.",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="">
-          <Navbar />
-          {children}
-          <Footer />
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="light">
+            <body className={inter.className}>
+                <div className="">
+                    <Navbar />
+                    {children}
+                    <Footer />
+                </div>
+            </body>
+        </html>
+    );
 }
