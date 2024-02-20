4956c7b60f33937726fb55ba2b45f9079c0d6b7b
Pasindu Geevinda <105873753+LuciferDIot@users.noreply.github.com>
2024-02-20 20:22:19 +0530

    signup page zod validation added

--- src/app/components/Login/Login.jsx
+++ src/app/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { loginSchema } from "@/zodSchema/login";
+import { SignUpSchema } from "@/lib/zodSchema/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -12,7 +12,7 @@ export default function Login() {
         register,
         getValues,
         formState: { errors, isSubmitting, isDirty, isInvalid },
-    } = useForm({ resolver: zodResolver(loginSchema) });
+    } = useForm({ resolver: zodResolver(SignUpSchema) });
 
     const fieldsChanging = (event) => {
         const email = getValues("email");
@@ -155,16 +155,16 @@ export default function Login() {
                             <path
                                 d="M15.0007 12C15.0007 13.6569 13.6576 15 12.0007 15C10.3439 15 9.00073 13.6569 9.00073 12C9.00073 10.3431 10.3439 9 12.0007 9C13.6576 9 15.0007 10.3431 15.0007 12Z"
                                 stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                             />
                             <path
                                 d="M12.0012 5C7.52354 5 3.73326 7.94288 2.45898 12C3.73324 16.0571 7.52354 19 12.0012 19C16.4788 19 20.2691 16.0571 21.5434 12C20.2691 7.94291 16.4788 5 12.0012 5Z"
                                 stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                             />
                         </svg>
                     ) : (
@@ -181,9 +181,9 @@ export default function Login() {
                                     id="Vector"
                                     d="M3.99989 4L19.9999 20M16.4999 16.7559C15.1473 17.4845 13.6185 17.9999 11.9999 17.9999C8.46924 17.9999 5.36624 15.5478 3.5868 13.7788C3.1171 13.3119 2.88229 13.0784 2.7328 12.6201C2.62619 12.2933 2.62616 11.7066 2.7328 11.3797C2.88233 10.9215 3.11763 10.6875 3.58827 10.2197C4.48515 9.32821 5.71801 8.26359 7.17219 7.42676M19.4999 14.6335C19.8329 14.3405 20.138 14.0523 20.4117 13.7803L20.4146 13.7772C20.8832 13.3114 21.1182 13.0779 21.2674 12.6206C21.374 12.2938 21.3738 11.7068 21.2672 11.38C21.1178 10.9219 20.8827 10.6877 20.4133 10.2211C18.6338 8.45208 15.5305 6 11.9999 6C11.6624 6 11.3288 6.02241 10.9999 6.06448M13.3228 13.5C12.9702 13.8112 12.5071 14 11.9999 14C10.8953 14 9.99989 13.1046 9.99989 12C9.99989 11.4605 10.2135 10.9711 10.5608 10.6113"
                                     stroke="currentColor"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 />
                             </g>
                         </svg>
--- src/zodSchema/login.js
+++
@@ -1,7 +0,0 @@
-import z from 'zod'
-
-export const loginSchema = z.object({
-    email: z.string().email(),
-    username: z.string().min(6),
-    password: z.string().min(5)
-})
