484e45a215743c17bc6d6aaeab298834e32f33fb
Anuradha godakanda <55067894+AnuradhaGP@users.noreply.github.com>
2024-02-25 18:41:13 +0530

    dashboard design Done

--- src/app/(seller)/dashboard/page.jsx
+++ src/app/(seller)/dashboard/page.jsx
@@ -5,7 +5,8 @@ import Link from 'next/link';
 export default function Dashboard() {
   return (
       <>
-       {/* profile pane */}
+      <div className="wrapper flex mx-1 my-4">
+        {/* profile pane */}
         <div className="profile-pane w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-10 ml-4">
           <div className="head flex flex-col items-center pb-10">
             <h2 className="mb-1 text-xl text-gray-900 dark:text-white items-center mb-5 font-semibold">Seller Profile</h2>
@@ -14,8 +15,8 @@ export default function Dashboard() {
                 <Image width={100} height={100}  src="/avatar.png" alt="profile image" />
               </div>
             </div>
-            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
-            <span class="text-sm text-gray-500 dark:text-gray-400 hover:font-semibold"><Link href="">Edit Profile</Link> </span>
+            <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+            <span className="text-sm text-gray-500 dark:text-gray-400 hover:font-semibold"><Link href="">Edit Profile</Link> </span>
           </div>
 
           <div className="account head flex flex-col items-left pb-10">
@@ -61,6 +62,84 @@ export default function Dashboard() {
           </div>
 
         </div>
+        {/* profile panne ends here */}
+
+        {/* adverticement pane */}
+        <div className="adverticement mx-4 grow">
+          
+          <h1  className=" text-right text-4xl font-bold">Advertisement</h1>
+
+          <div className="relative overflow-x-auto shadow-md sm:rounded-lg my-4 p-2">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <caption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                    History
+                    <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">All your products are here. </p>
+                </caption>
+                <thead className="text-xs text-gray-500  bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3 rounded-s-lg text-gray-700 ">
+                            Title
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Price
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Location
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Predicted Price
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Action
+                        </th>
+                        <th scope="col" className="px-6 py-3 rounded-e-lg">
+                            More
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <div>Sample Ad-Title</div>
+                            <span className=" text-xs text-gray-400 py-2">
+                              Date
+                            </span>
+                        </th>
+                        <td className="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                            <div>$0,000,000.00</div>
+                            <span className=" text-xs text-gray-400 py-2">
+                              Pending
+                            </span>
+                        </td>
+                        <td className="px-6 py-4 text-gray-900 dark:text-white">
+                            <div>Sample Ad-Location</div>
+                            <span className=" text-xs text-gray-400 py-2">
+                              Address
+                            </span>
+                        </td>
+                        <td className="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                            <div>$0,000,000.00</div>
+                            <span className=" text-xs text-gray-400 py-2">
+                              Accepted
+                            </span>
+                        </td>
+                        <td className="px-6 py-4 text-right text-gray-900 whitespace-nowrap dark:text-white">
+                            <a href="#" className="font-medium text-white dark:text-white bg-lime-700 px-3 py-1 rounded-sm hover:bg-lime-800">Edit</a>
+                        </td>
+                        <td className="px-6 py-4 text-center">
+                          <svg className="hover:text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                          </svg>
+                        </td>
+                    </tr>
+                   
+                </tbody>
+            </table>
+          </div>
+
+        </div>
+      </div>
+
       </>
     );
 }
