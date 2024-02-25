75226c3d577dca4b10f9d4bf8a152cc7ba76cf9d
Movindu Gunarathna <movindugunarathna@gmail.com>
2024-02-25 19:05:03 +0530

    db integration started

---
+++ src/models/advertisementModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const advertisementSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+    },
+    locationURL: {
+        type: String,
+        required: [true, "Location is required"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Price is required"],
+    },
+    image: {
+        type: String,
+        required: [true, "Image is required"],
+    },
+    isSold: {
+        type: Boolean,
+        default: false,
+    },
+    creationdDate: {
+        type: Date,
+        default: Date.now,
+    },
+    modifiedDate: {
+        type: Date,
+        default: Date.now,
+    },
+    tags: {
+        type: [String],
+        enum: ["work", "lifestyle", "motor", "mobile"],
+    },
+},
+    {
+        timestamps: true,
+    }
+);
+
+const Advertisement = mongoose.models.Advertisement || mongoose.model("Advertisement", advertisementSchema);
+
+export default Advertisement;
