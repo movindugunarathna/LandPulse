0ea5d512cfe35184d84dc8b0e69c28ea21ceb398
Movindu Gunarathna <movindugunarathna@gmail.com>
2024-03-04 22:07:17 +0530

    create ad modal layout done

--- src/app/(seller)/[sellerId]/(ad)/create/page.jsx
+++ src/app/(seller)/[sellerId]/(ad)/create/page.jsx
@@ -1,110 +1,95 @@
-"use client";
-import Dropzone from "@/app/components/Dropzone/dropzone";
-import PriceSection from "@/app/components/PriceSection/page";
-import { useState } from "react";
+'use client';
+import Dropzone from '@/app/components/Dropzone/dropzone';
+import GoogleMapComp from '@/app/components/GoogleMap/GoogleMapSearch';
+import PriceSection from '@/app/components/PriceSection/page';
+import { useState } from 'react';
 
 export default function CreateAd() {
-    const [priceSection, setPriceSection] = useState({
-        selected: false,
-        value: 0.0,
-    });
+  const [priceSection, setPriceSection] = useState({
+    selected: false,
+    value: 0.0,
+  });
 
-    return (
-        <>
-            {priceSection.selected && (
-                <PriceSection
-                    setPriceDetails={setPriceSection}
-                    priceDetails={priceSection}
-                />
-            )}
-            <h2 className="text-3xl font-bold text-gray-800 mt-10 ml-10">
-                Create Advertisement
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-8 rounded-lg">
-                <div className="text-center">
-                    <Dropzone className="p-5 mt-2 border border-neutral-200" />
-                </div>
-                <form action="/submit_form" method="post">
-                    <div className="mb-4">
-                        <label
-                            htmlFor="title"
-                            className="block text-gray-700 font-bold mb-2 text-left "
-                        >
-                            Title:
-                        </label>
-                        <input
-                            type="text"
-                            id="title"
-                            name="title"
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-5"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label
-                            htmlFor="description"
-                            className="block text-gray-700 font-bold mb-2 text-left"
-                        >
-                            Description:
-                        </label>
-                        <textarea
-                            id="description"
-                            name="description"
-                            rows="5"
-                            cols="50"
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-5"
-                        ></textarea>
-                    </div>
-                    <div className="grid grid-cols-1 md:grid-cols-2 mb-4">
-                        <div className="mb-4 md:mr-5">
-                            <label
-                                htmlFor="price"
-                                className="block text-gray-700 font-bold mb-2 text-left"
-                            >
-                                Price:
-                            </label>
-                            <input
-                                type="text"
-                                id="price"
-                                name="price"
-                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight 
+  return (
+    <>
+      {priceSection.selected && (
+        <PriceSection
+          setPriceDetails={setPriceSection}
+          priceDetails={priceSection}
+        />
+      )}
+      <h2 className="text-3xl font-bold text-gray-800 mt-10 ml-10">
+        Create Advertisement
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-8 rounded-lg">
+        <div className="text-center">
+          <Dropzone className="p-5 mt-2 border border-neutral-200" />
+        </div>
+        <form action="/submit_form" method="post">
+          <div className="mb-4">
+            <label
+              htmlFor="title"
+              className="block text-gray-700 font-bold mb-2 text-left "
+            >
+              Title:
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-5"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="description"
+              className="block text-gray-700 font-bold mb-2 text-left"
+            >
+              Description:
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              rows="5"
+              cols="50"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-5"
+            ></textarea>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 mb-4">
+            <div className="mb-4 md:mr-5">
+              <label
+                htmlFor="price"
+                className="block text-gray-700 font-bold mb-2 text-left"
+              >
+                Price:
+              </label>
+              <input
+                type="text"
+                id="price"
+                name="price"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight 
                                 focus:outline-none focus:shadow-outline mb-5 bg-slate-200 cursor-pointer"
-                                readOnly={true}
-                                value={`Rs. ${priceSection.value}`}
-                                onClick={() => {
-                                    console.log("Selected price");
-                                    setPriceSection({
-                                        ...priceSection,
-                                        selected: true,
-                                    });
-                                }}
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label
-                                htmlFor="landType"
-                                className="block text-gray-700 font-bold mb-2 text-left"
-                            >
-                                Land Type:
-                            </label>
-                            <select
-                                id="landType"
-                                name="landType"
-                                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            >
-                                <option value="commercial">Commercial</option>
-                                <option value="residential">Residential</option>
-                            </select>
-                        </div>
-                    </div>
-                    <button
-                        id="publish"
-                        name="publish"
-                        className="bg-custom-green-100 hover:bg-lime-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    >
-                        Publish
-                    </button>
-                </form>
+                readOnly={true}
+                value={`Rs. ${priceSection.value}`}
+                onClick={() => {
+                  console.log('Selected price');
+                  setPriceSection({
+                    ...priceSection,
+                    selected: true,
+                  });
+                }}
+              />
             </div>
-        </>
-    );
+          </div>
+          <button
+            id="publish"
+            name="publish"
+            className="bg-custom-green-100 hover:bg-lime-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Publish
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
--- src/app/components/PriceSection/components/PredictPrice.jsx
+++ src/app/components/PriceSection/components/PredictPrice.jsx
@@ -1,41 +1,69 @@
-import React, { useState } from "react";
-import { landTypes } from "@/data/landTypes";
+import React, { useState } from 'react';
+import { landTypes } from '@/data/landTypes';
+import GoogleMapComp from '../../GoogleMap/GoogleMapSearch';
 
 export default function PredictPrice() {
-    const [selectedLandTypes, setSelectedLandTypes] = useState([]);
+  const [selectedLandTypes, setSelectedLandTypes] = useState([]);
 
-    return (
-        <div className="w-full h-full">
-            <label
-                htmlFor="price"
-                className="block text-gray-700 font-bold mb-2 text-left"
-            >
-                Land Type:
-            </label>
-            <div className=" w-full flex justify-start items-center gap-4">
-                {landTypes.map((item) => (
-                    <input
-                        key={item.value}
-                        type="button"
-                        className={`inline-flex items-center justify-center px-4 py-1 font-sans font-semibold tracking-wide 
-              border ${selectedLandTypes.includes(item.type) ? "bg-gray-400" : "border-black"} rounded-lg`}
-                        value={item.type}
-                        onClick={() => {
-                            if (selectedLandTypes.includes(item.type)) {
-                                setSelectedLandTypes(
-                                    selectedLandTypes.filter(
-                                        (type) => type !== item.type
-                                    )
-                                );
-                            } else
-                                setSelectedLandTypes([
-                                    ...selectedLandTypes,
-                                    item.type,
-                                ]);
-                        }}
-                    />
-                ))}
-            </div>
+  return (
+    <div className="">
+      <label
+        htmlFor="price"
+        className="block text-gray-700 font-bold mb-2 text-left"
+      >
+        Land Type:
+      </label>
+      <div className=" w-full flex justify-start items-center gap-4">
+        {landTypes.map((item) => (
+          <input
+            key={item.value}
+            type="button"
+            className={`inline-flex items-center justify-center px-4 py-1 font-sans font-semibold tracking-wide 
+                                                        border ${selectedLandTypes.includes(item.type) ? 'bg-gray-400' : 'border-black'} rounded-lg`}
+            value={item.type}
+            onClick={() => {
+              if (selectedLandTypes.includes(item.type)) {
+                setSelectedLandTypes(
+                  selectedLandTypes.filter((type) => type !== item.type)
+                );
+              } else setSelectedLandTypes([...selectedLandTypes, item.type]);
+            }}
+          />
+        ))}
+      </div>
+      <label
+        htmlFor="location"
+        className="block text-gray-700 font-bold mt-5 text-left"
+      >
+        Pick the Location:
+      </label>
+      <div className="grid grid-cols-2 gap-10">
+        <div className="">
+          <GoogleMapComp className="w-full h-96 mt-2" />
         </div>
-    );
+        <div>
+          <div className="">
+            <label
+              htmlFor="predicted-price"
+              className="block text-gray-700 font-bold mt-5 text-left"
+            >
+              Predicted Price
+            </label>
+            <div className="text-2xl font-bold text-black">Rs {}/=</div>
+            <div className="mt-5">
+              <label
+                htmlFor="price-range"
+                className="block text-gray-700 font-bold mt-5 text-left"
+              >
+                Price Range
+              </label>
+              <div className="text-xl font-bold text-black">
+                Rs {} - Rs {}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
