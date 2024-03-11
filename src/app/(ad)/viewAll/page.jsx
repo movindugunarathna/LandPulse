2dddcf486a779f7df08db5d151b1cb939355146b
Pasindu Geevinda <105873753+LuciferDIot@users.noreply.github.com>
2024-03-11 20:59:39 +0530

    Update page.jsx

--- src/app/(ad)/viewAll/page.jsx
+++ src/app/(ad)/viewAll/page.jsx
@@ -29,7 +29,7 @@ const ViewAll = () => {
             Loading...
         </div>
     ) : (
-        <div className="min-h-screen min-w-full flex flex-col gap-10 overflow-x-hidden">
+        <div className="min-h-screen min-w-full flex flex-col justify-center items-center gap-10 overflow-x-hidden">
             {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sort</button> */}
             <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5 mx-52">
                 {advertisements.map((advertisement) => (
