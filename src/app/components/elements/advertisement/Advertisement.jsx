b986cb380f850a4e4c997bccd8912d84e1a4b9ae
Pasindu Geevinda <105873753+LuciferDIot@users.noreply.github.com>
2024-03-11 20:59:07 +0530

    view all and home page done

--- src/app/components/elements/advertisement/Advertisement.jsx
+++ src/app/components/elements/advertisement/Advertisement.jsx
@@ -1,19 +1,40 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
 
 const Advertisement = ({ advertisement }) => {
-  return (
-    <div className="bg-white shadow-md p-6 rounded-lg">
-      <div key={advertisement._id} className="mb-4">
-        <Image src={advertisement.image} alt={advertisement.image} width={500} height={300} className="rounded-md" />
-        <div className="text-lg font-medium mb-2">{advertisement.landType}</div>
-        <div className="text-gray-500 mb-4">
-          <span className="text-gray-800 text-sm">Price per perch </span>LKR {advertisement.price}
-        </div>
-        <h2 className="text-2xl font-bold mb-2">{advertisement.title}</h2>
-        <p className="text-gray-700 mb-4">{advertisement.description}</p>
-      </div>
-    </div>
-  );
+    return (
+        <Link
+            className="max-h-96 h-fit bg-white shadow-xl backdrop-blur-md p-6 rounded-lg  text-left overflow-hidden cursor-pointer hover:bg-slate-100"
+            href={`viewAd/${advertisement._id}`}
+        >
+            <div
+                key={advertisement._id}
+                className="flex justify-between items-center flex-col"
+            >
+                <Image
+                    src={advertisement.images[0].url}
+                    alt={advertisement.images[0].url}
+                    width={500}
+                    height={500}
+                    className="rounded-md h-52"
+                />
+
+                <h2 className=" w-full font-bold mb-2">
+                    {advertisement.title}
+                </h2>
+                <div className=" w-full text-gray-500 flex flex-col text-sm">
+                    {advertisement.landTypes.join(", ")}
+                    <p className="w-full flex justify-between items-center text-gray-800 text-sm">
+                        Price per perch{" "}
+                        <span>
+                            LKR {Number(advertisement.price).toFixed(2)} /=
+                        </span>
+                    </p>
+                </div>
+                <p className="text-gray-700">{advertisement.landType}</p>
+            </div>
+        </Link>
+    );
 };
 
 export default Advertisement;
