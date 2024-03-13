863b0da1f8d62115058fcee330cfcfee4433eb32
Anuradha godakanda <55067894+AnuradhaGP@users.noreply.github.com>
2024-03-14 04:38:53 +0530

    updated Profile page

--- src/app/(seller)/dashboard/page.jsx
+++ src/app/(seller)/dashboard/page.jsx
@@ -2,10 +2,11 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { redirect,useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import { signOut, useSession } from "next-auth/react";
 import { getUserByID } from "@/actions/userActions";
 import { useState } from "react";
+import path from "path";
 
 export default function Dashboard({ userData }) {
   const { data: session, status } = useSession();
@@ -53,14 +54,11 @@ export default function Dashboard({ userData }) {
                 {user?.username || "Fetching..."}
               </h5>
               <span className="text-sm text-gray-500 dark:text-gray-400 hover:font-semibold">
-                <button
-                  onClick={() => {
-                    console.log("Redirecting to edit page");
-                    router.push("/profile");
-                  }}
+                <Link
+                  href={ "/profile"}
                 >
                   Edit Profile
-                </button>
+                </Link>
               </span>
             </div>
 
