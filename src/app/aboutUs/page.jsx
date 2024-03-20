11bb122981973700878e3b9bda59dfe3c138b5b1
Nisandu01 <senanayakenisandu456@gmail.com>
2024-03-20 18:33:19 +0530

    terms added

--- src/app/aboutUs/page.jsx
+++ src/app/aboutUs/page.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const Page = () => {
   return (
     <div>
@@ -26,7 +25,11 @@ const Page = () => {
             <p>Our search and filters make it super easy to find exactly what you're looking for!</p>
           </div>
         </div>
-        <div id="contact-info" className="w-full p-4 bg-gray-200 shadow-md mx-auto">
+        <div id="terms" className="w-full p-4 mx-auto"> {/* Added mx-auto class */}
+          <h2 className="text-xl font-bold">Our Terms and conditions</h2>
+          <p>By accessing or using LandPulse, you agree to abide by the following terms and conditions. You may use LandPulse for personal, non-commercial purposes only and must comply with all applicable laws and regulations. Users are responsible for the accuracy and legality of their uploaded content and must not violate intellectual property rights or upload objectionable material. LandPulse collects and uses personal information in accordance with its Privacy Policy. LandPulse and its suppliers are not liable for any damages arising from the use of the platform. These terms are governed by the laws of Sri Lanka, and users agree to the exclusive jurisdiction of the courts in that jurisdiction. LandPulse may revise these terms at any time, and continued use constitutes acceptance of the current terms and conditions.</p>
+        </div>
+        <div id="contact-info" className="w-full p-4 bg-gray-200 shadow-md mx-auto"> {/* Added mx-auto class */}
           <h2 className="text-xl font-bold">Questions? Get in touch!</h2>
           <p>9am - 6pm on weekdays</p>
           <p>8am - 5pm on weekends and mercantile holidays</p>
